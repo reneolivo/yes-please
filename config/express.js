363cdd156ddce1671bce711b186dde334bbef40b
René Olivo <minet.ws@gmail.com>
2014-02-18 23:05:22 -0400

    bootstrap less

--- config/express.js
+++ config/express.js
@@ -1,17 +1,31 @@
 var express = require('express');
+var lessMiddleware = require('less-middleware');
 
 module.exports = function(app, config) {
   app.configure(function () {
     app.use(express.compress());
-    app.use(express.static(config.root + '/public'));
     app.set('port', config.port);
     app.set('views', config.root + '/app/views');
     app.set('view engine', 'jade');
-    app.use(express.favicon(config.root + '/public/img/favicon.ico'));
+    app.use('view options', { pretty: true });
+    //app.use(express.favicon(config.root + '/public/img/favicon.ico'));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
+    //app.use('')
+
+    app.use(lessMiddleware({
+        src         : config.root + '/public/less/',
+        dest        : config.root + '/public/css/',
+        prefix      : '/css',
+        paths       : [ config.root + '/components/bootstrap/less/bootstrap.less' ],
+        compress    : false,
+        debug       : false
+    }));
+
+    app.use(express.static(config.root + '/public'));
+
     app.use(function(req, res) {
       res.status(404).render('404', { title: '404' });
     });
